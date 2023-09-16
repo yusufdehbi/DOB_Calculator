bb2f8ca89fdb6ee8132fbd2825798d4c2442a1f4
yusufdehbi <63043024+yusufdehbi@users.noreply.github.com>
2023-09-16 11:55:12 +0100

    working with linearLayout

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Date_Of_Birth_Calculator"
+include(":app")
+ 
